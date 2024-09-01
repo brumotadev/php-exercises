fa53ea3f2650c64a5fa9e25ad73c790184c35fdc
Bruno Mota <bmotadev@gmail.com>
2024-09-01 19:27:42 -0300

    :recycle: refactor: alterado o enunciado do exercicio 1006

--- exercise1006.php
+++ exercise1006.php
@@ -1,9 +1,14 @@
 <?php 
 
 /**
- * Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. 
- * A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). 
- * Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+ * Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. 
+ * A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. 
+ * Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
  */
+
+ $valorA = trim(fgets(STDIN));
+ $valorB = trim(fgets(STDIN));
+
+ 
 
 ?>
