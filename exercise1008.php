a01b39058a26197efda148d505f4c3502aa3f21b
Bruno Mota <bmotadev@gmail.com>
2024-09-02 21:47:12 -0300

    :sparkles: feat: exercise 1008

---
+++ exercise1008.php
@@ -0,0 +1,6 @@
+<?php
+
+// Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. 
+// A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+
+?>
