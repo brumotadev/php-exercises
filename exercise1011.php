07ca1131152173f639466bc8145378d7d19805ee
Bruno Mota <bmotadev@gmail.com>
2024-09-03 22:28:50 -0300

    :sparkles: feat: exercise 1011 - resolvido

--- exercise1011.php
+++ exercise1011.php
@@ -5,4 +5,12 @@
  * A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
  */
 
+ $valorDoRaio = trim(fgets(STDIN));
+
+ $valorDoPi = 3.14159;
+
+ $volume = (4/3.0) * $valorDoPi * (pow($valorDoRaio, 3));
+
+ printf("VOLUME = %.3f\n", $volume);
+
 ?>
