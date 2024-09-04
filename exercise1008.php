04ec8d9b67cfd811bb224b62866712b5221ea7f2
Bruno Mota <bmotadev@gmail.com>
2024-09-03 21:26:13 -0300

    :sparkles: feat: exercise 1008 - resolvido

--- exercise1008.php
+++ exercise1008.php
@@ -3,4 +3,13 @@
 // Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. 
 // A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
 
+$numeroFuncionario = trim(fgets(STDIN));
+$numeroDeHorasTrabalhadas = trim(fgets(STDIN));
+$valorPorHora = trim(fgets(STDIN));
+
+$salario = $numeroDeHorasTrabalhadas * $valorPorHora;
+
+printf("NUMBER = %d\n", $numeroFuncionario);
+printf("SALARY = U$ %.2f\n", $salario);
+
 ?>
