df0d451860affc63191d29750b2b0b4046d3d18d
Bruno Mota <bmotadev@gmail.com>
2024-09-03 21:34:42 -0300

    :sparkles: feat: exercise 1009 - resolvido

--- exercise1009.php
+++ exercise1009.php
@@ -3,4 +3,15 @@
 // Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). 
 // Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, informar o total a receber no final do mês, com duas casas decimais.
 
+$nomeDoVendedor = trim(fgets(STDIN));
+$salarioFixo = trim(fgets(STDIN));
+$totalDeVendasMes = trim((fgets(STDIN)));
+$comissao = 15/100; // 0.15
+
+$comissao = $totalDeVendasMes * $comissao; // total de vendas * 0.15
+
+$salarioTotalMes = $comissao + $salarioFixo;
+
+printf("TOTAL = R$ %.2f\n", $salarioTotalMes);
+
 ?>
